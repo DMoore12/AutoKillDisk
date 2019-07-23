fcaecf4128f32f241acaa43ee8a573a5f32dc1e8
Dawson Moore <moore800@purdue.edu>
2019-07-23 16:30:11 -0500

    Add initial changes to new GUI separation

--- autoKillDisk.py
+++ autoKillDisk.py
@@ -1,22 +1,12 @@
 import sys
-import os
 import random
-from PySide2.QtWidgets import (QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QGridLayout, QLineEdit, QSpacerItem, QRadioButton, QGroupBox, QProgressBar)
-from PySide2 import QtCore
-#from PySide2.QtCore import Slot, Qt, QThread, QObject
-#import psutil
-#from psutil._common import bytes2human
 import wmi
 import subprocess
 import time
-from threading import Timer, Thread
 import pythoncom
-
-# Possible fix to the threading issue: https://stackoverflow.com/questions/36434706/pyqt-proper-use-of-emit-and-pyqtsignal
-# Also, checkout fbs installer
-# Also, https://pythonprogramming.net/menubar-pyqt-tutorial/?completed=/button-functions-pyqt-tutorial/
-# Also, possible fix two: https://www.riverbankcomputing.com/static/Docs/PyQt5/signals_slots.html
-# TODO: Create a proper build script
+from PySide2.QtWidgets import (QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QGridLayout, QLineEdit, QRadioButton, QProgressBar)
+from PySide2 import QtCore
+from threading import Timer, Thread
 
 class mainScreen(QWidget):
     def __init__(self):
@@ -31,15 +21,9 @@ class mainScreen(QWidget):
         self.progress = QProgressBar()
         self.wipeButton = QPushButton('Wipe')
         self.cancelButton = QPushButton('Cancel')
-        self.bottomStatus = QLabel('Ready to Wipe')
-        self.refactor = QPushButton('Refactor')
-
-        # TODO: Add a group box to make this look better
-        # EDIT TODO: Use GUI application to build a really good looking app!
-        # TODO: Add selection of wipe method, verification, and certificate output
+        self.bottomStatus = QLabel('Load Complete')
 
         self.layout = QGridLayout()
-
         self.layout.addWidget(self.driveLabel, 0, 0, 1, 2)
         self.layout.addWidget(self.sizeLabel, 0, 3)
         self.layout.addWidget(self.statusLabel, 0, 5, 1, 2)
@@ -47,10 +31,9 @@ class mainScreen(QWidget):
         self.layout.addWidget(self.indexLabel, 0, 8)
         self.layout.addWidget(self.checkLabel, 0, 9)
         self.layout.addWidget(self.progress, 26, 0, 1, 10)
-        self.layout.addWidget(self.wipeButton, 27, 2, 1, 2)
-        self.layout.addWidget(self.cancelButton, 27, 4, 1, 2)
+        self.layout.addWidget(self.wipeButton, 27, 0, 1, 2)
+        self.layout.addWidget(self.cancelButton, 27, 2, 1, 2)
         self.layout.addWidget(self.bottomStatus, 28, 0, 1, 10)
-        self.layout.addWidget(self.refactor, 27, 0, 1, 2)
         self.drivesSet = 0
         self.driveNames = []
         self.driveStatus = []
@@ -58,7 +41,6 @@ class mainScreen(QWidget):
         self.drivePartitions = []
         self.driveIndex = []
         self.masterRadio = []
-        self.master = self.getMaster()
         self.progressInt = 10
         for i in range(25):
             toAdd = QLabel('')
@@ -71,18 +53,24 @@ class mainScreen(QWidget):
             self.drivePartitions.append(toAdd4)
             toAdd5 = QLabel('')
             self.driveIndex.append(toAdd5)
-
         self.setWindowTitle('Auto Kill Disk')
-        #icon = 
-        #self.setWindowIcon()
-        self.wipeButton.clicked.connect(self.startButtonClicked)
-        self.refactor.clicked.connect(self.refactorDrives)
 
-        self.worker = refactorThread()
-        self.worker.refSig.connect(self.refactorDrives)
-        self.worker.start()
+        #TODO: Write the startWipe() function outside the class to remove from Qt GUI thread
+
+        self.wipeButton.clicked.connect(startWipe)
+
+        #TODO: Write the cancelWipe() function
+
+        self.cancelButton.clicked.connect(cancelWipe)
+
+        #TODO: Add threading call here
+        initThread = initThread()
+        self.initThread.connect(self.setMaster)
 
     def addDrive(self, name, status, size, partitions, index):
+
+        #TODO: Rewrite this function to work from a non GUI thread and add bool for master
+
         self.driveNames[self.drivesSet].setText(name)
         self.driveStatus[self.drivesSet].setText(status)
         self.driveSize[self.drivesSet].setText(size + ' GB')
@@ -93,7 +81,7 @@ class mainScreen(QWidget):
         self.layout.addWidget(self.masterRadio[self.drivesSet], self.drivesSet + 1, 9)
         if int(index) == int(self.master):
             self.masterRadio[self.drivesSet].setChecked(True)
-        self.drivesSet += 1       
+        self.drivesSet += 1  
 
     def addPayloadNames(self):
         for i in range(25):
@@ -106,65 +94,12 @@ class mainScreen(QWidget):
     def resetSpacing(self):
         self.layout.setContentsMargins(10, 10, 0, 10)
 
-    def startButtonClicked(self):
-        self.bottomStatus.setText('Are you sure you want to wipe?')
-        self.confirmButton = QPushButton('Confirm')
-        self.layout.addWidget(self.confirmButton, 27, 6, 1, 2)
-        self.setLayout(self.layout)
+    #TODO: Add the signal and slot to make this call work properly
 
-        self.confirmButton.clicked.connect(self.getIndex)
+    def setText(self, text):
+        self.bottomStatus.setText(text)
 
-    def setText(self):
-        self.bottomStatus.setText('Ready to Wipe')
-
-    def getIndex(self):
-        self.confirmButton.deleteLater()
-        self.bottomStatus.setText('Starting Wipe')
-        self.indexToWipe = []
-        self.serialToWipe = []
-        for i in range(len(self.masterRadio)):
-            if not self.masterRadio[i].isChecked():
-                self.indexToWipe.append(self.index[i])
-                self.serialToWipe.append(self.serial[i])
-        if len(self.indexToWipe) == len(self.index):
-            self.bottomStatus.setText('Error: No master drive selected!')
-            self.refactorDrives()
-            t = Timer(3, self.setText)
-            t.start()
-            return None
-        elif len(self.indexToWipe) == 0:
-            self.refactorDrives()
-            self.bottomStatus.setText('No drives available to wipe!')
-            t = Timer(3, self.setText)
-            t.start()
-        else:
-            serialString = self.serialToWipe[0]
-            for i in range(1, len(self.serialToWipe)):
-                serialString += ', ' + self.serialToWipe[i]
-            self.bottomStatus.setText('Wiping Drives:' + serialString)
-            self.progress.setValue(self.progressInt)
-            for i in range(len(self.masterRadio)):
-                # TODO: Figure out which index is the master so we don't wipe it
-                if self.masterRadio[i].isChecked():
-                    self.driveStatus[i].setText('MASTER')
-                else:
-                    self.driveStatus[i].setText('WIPING')
-            wipeDrives(self, self.indexToWipe)
-
-    def refactorDrives(self):
-        pythoncom.CoInitialize()
-        for i in range(len(self.driveNames)):
-            self.driveNames[i].setText('')
-            self.driveStatus[i].setText('')
-            self.driveSize[i].setText('')
-            self.drivePartitions[i].setText('')
-            self.driveIndex[i].setText('')
-        for i in range(len(self.masterRadio)):
-            self.masterRadio[i].deleteLater()
-        self.setLayout(self.layout)
-        del self.masterRadio
-        self.masterRadio = []
-        self.drivesSet = 0
+    def resetLabels(self):
         for i in range(25):
             toAdd = QLabel('')
             self.driveNames.append(toAdd)
@@ -178,93 +113,52 @@ class mainScreen(QWidget):
             self.driveIndex.append(toAdd5)
         self.addPayloadNames()
         self.setLayout(self.layout)
-        getDisks(self)
-        self.addPayloadNames()
-        self.setLayout(self.layout)
 
-    def getMaster(self):
-        exists = os.path.isfile('config.txt')
-        if not exists:
-            self.bottomStatus.setText('Error: Config file not found. Defaulting to master drive with index 0.')
-            t = Timer(5, self.setText)
-            t.start()
-            return 0
-        with open('config.txt', 'r') as fh:
-            for line in fh:
-                toSet = line.strip(' ')
-        self.bottomStatus.setText('Loading complete')
-        t = Timer(5, self.setText)
-        t.start()
-        return toSet
+    #TODO: Add the signal and slot to make the master set with logic outside the GUI thread
+    #TODO: Remember that the index for the masterRadio list and that something neeeds to be done outside the GUI thread to get by this
+    #TODO: It could also be done where each drive has a master status when it gets added, but that should be done later
 
-class refactorThread(QtCore.QThread):
-    refSig = QtCore.Signal()
+    def setMaster(self, masterInt):
+        try:
+            self.masterRadio[masterInt].setChecked()
+        except:
+            print('The given master was outside the range of given ones')
+
+class initial(Thread):
+    masterFoundSig = QtCore.Signal(int)
 
     def __init__(self):
-            QtCore.QThread.__init__(self)
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
 
     def run(self):
-            while True:
-                self.refSig.emit()
-                # Make sure the time doesn't go much lower than 1 or the application will stop responding
-                time.sleep(1)
+        # All initialization tasks not related to the GUI should go here
+        try:
+            with open('config.txt') as fh:
+                for line in fh:
+                    master = int(line)
+            self.masterFoundSig.emit(master)
+        except:
+            print('Config file was unable to be opened!')
 
-#Comes from https://github.com/giampaolo/psutil/blob/master/scripts/disk_usage.py
-def getDisks(window):
-    counter = 0
-    window.index = []
-    window.serial = []
-    c = wmi.WMI()
-    for pm in c.Win32_DiskDrive():
-        #print(pm.SerialNumber, pm.Name, pm.Size, pm.Status, pm.SystemName, pm.Signature, pm.Partitions)
-        window.addDrive(pm.SerialNumber.strip(' '), pm.Status, str(round( (int(pm.Size) / 1000000000), 2)), pm.Partitions, pm.Index)
-        window.index.append(pm.Index)
-        window.serial.append(pm.SerialNumber.strip(' '))
+class refactor(Thread):
 
-def wipeDrives(parent, indexList):
-    print('Running')
-    initialFiles = fileCount()
-    increment = 90 / len(indexList)
-    for index in indexList:
-        #string = 'KillDisk -erasemethod=[3] -passes=[3] -verification=[10] -retryattempts=[5] -wipehdd=[' + index.strip(' ') + '] -noconfirmation'
-        string = 'C:/"Program Files"/"LSoft Technologies"/"Active@ KillDisk Ultimate 11"/KillDisk.exe -erasemethod=2 -passes=3 -verification=25 -retryattempts=5 -erasehdd=' + str(index) + ' -cp=C:/Users/%USERNAME%/Desktop/"Certificate Output"/ -nc -bm\n'
-        p = subprocess.Popen(string, stdout = subprocess.PIPE, shell = True)
-        (output, err) = p.communicate()
-        p_status = p.wait()
-        files = fileCount()
-        if not files == initialFiles:
-            self.progressInt += increment
-            self.progress.setValue(self.progressInt)
-            initialFiles += 1
-        else:
-            toBreak = False
-            while not toBreak:
-                time.sleep(3000)
-                files = fileCount()
-                if fileCount == initialFiles:
-                    self.progressInt += increment
-                    self.progress.setValue(self.progressInt)
-                    initialFiles += 1
-                    toBreak = True
-                    break
+    def __init(self):
+        Thread.__init__(self)
+        self.daemon = True
 
-def fileCount():
-    fileCount = 0
-    for _, dirs, files in os.walk('C:/Users/%USERNAME%/Desktop/"Certificate Output"/'):
-        fileCount += len(files)
-    return fileCount
-
+    def run(self):
+        #TODO: Add drive refactoring stuff
+        #NOTE: It should be fine to just get all the drives all the time, but it would be nice to gather some info so we don't have to do it every time
+            # Maybe just get the number of drives and compare?
+            # Then every so often just run the whole refactor in case someone pulls a fast one on us?
+        
 if __name__ == '__main__':
     main = QApplication(sys.argv)
     window = mainScreen()
-
-    getDisks(window)
 
     window.resetSpacing()
     window.addPayloadNames()
     window.setLayout(window.layout)
     window.setFixedSize(500, 600)
-
-    window.show()
-
-    sys.exit(main.exec_())
